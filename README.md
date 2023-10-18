b868fba81924108605a0ad351700aa20528404dd
user <sirojiddin1987@gmail.com>
2023-10-18 22:39:22 +0500

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-
+You can download assignments from github 
 [oop homework 1.pdf](https://github.com/NewUU-OOP/hw1/files/13025923/oop.homework.1.pdf)
---
+++ Test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+    public static void main(String[] args) {
+        // ADD CODE HERE //
+        // Should print the values in students in order
+        for (int row = 0; row <= students . length ; row ++)
+        {
+            for (int col = 0; col <= students . length ; col ++)
+            {
+                System . out . print ( students [row ][ col ] + " ");
+            }
+        }
+    }
+}
