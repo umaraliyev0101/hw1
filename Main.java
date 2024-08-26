2648a145902c4424853ce706272026f1d11f6dff
sdjuraev <115090257+sdjuraev@users.noreply.github.com>
2024-08-26 13:10:27 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,35 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
     }
-    public static void problem1(){
-        //Write your code here
-    }
-    public static void problem2(){
-        //Write your code here
-    }
-    public static void problem3(){
-        //Write your code here
-    }
-    public static void problem4(){
-        //Write your code here
-    }
-    public static void problem5(){
-        //Write your code here
-    }
-    public static void problem6(){
-        //Write your code here
-    }
-    public static void problem7(){
-        //Write your code here
-    }
+    
     public static int sumDigits(long n){
-        //Problem 8
+        
         //Write your code here
 
         return 0;
     }
-    public static boolean checkPassword(){
-        //Problem 9
+    public static boolean checkPassword(String password){
+        
         //Write your code here
         return true;
     }
@@ -41,16 +21,6 @@ public class Main {
     public static double min(double[] array){
         //Write your code here
         // problem 11
-        return 0;
-    }
-    public static boolean equals(int [] [] m1, int [] [] m2){
-        //Problem 12
-        return true;
-    }
-    public static void sort(int m[][]){
-
-    }
-    public static double getTriangleArea(double[][] points){
         return 0;
     }
 }
