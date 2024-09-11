2a852ba0de6bffffba01a575142676829a44e5d2
macbookair <s.djuraev@newuzbekistanuniversity.uz>
2024-09-12 02:10:40 +0500

    V1

---
+++ student/Homework1.java
@@ -0,0 +1,29 @@
+package student;
+
+public class Homework1 {
+
+    public double shippingCalculator(int n){
+        //TODO Write your code here for problem 1
+        return 0;
+    }
+    public boolean isValidTriangle(int a, int b, int c){
+        //TODO Write your code here for problem 2
+        return false;
+
+    }
+    public boolean isPrime(int number){
+        //TODO Write your code here for problem 3
+        return false;
+    }
+
+    public  int hex2int(String hex) {
+        //TODO Write your code here for problem 4
+        return 0;
+    }
+    public String int2hex(int value) {
+        //TODO Write your code here for problem 4
+        return "";
+    }
+
+
+}
